05157dd00db6a17056e974f0e18ef9b538aa6b62
thanhbinh2612 <thanhbinh261294@gmail.com>
2020-07-13 17:17:08 +0700

    Thực hành- Triển khai interface 'Comparator' để so sánh các lớp hình học

---
+++ Abstract Class & Interface/src/THComparator/Circle.java
@@ -0,0 +1,48 @@
+package THComparator;
+
+public class Circle {
+    protected double radius;
+    protected String color;
+    protected boolean filled;
+    public Circle(){
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled)
+    {
+        this.radius = radius;
+        this.color = color;
+        this.filled = filled;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public double getArea(){
+        return this.radius*this.radius*Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "A Circle with radius of "
+                + this.getRadius()
+                + " and area is: "
+                + getArea();
+
+    }
+}
