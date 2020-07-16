aea068ecc6673a3998141ecfe12251c0691904fa
thanhbinh2612 <thanhbinh261294@gmail.com>
2020-07-16 14:09:36 +0700

    Thực hành - Triển khai lớp LinkedList đơn giản

---
+++ DSA-List/test/simplelinkedlist/TestMyLinkedList.java
@@ -0,0 +1,38 @@
+package simplelinkedlist;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestMyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("/=/=/=/= TESTING /=/=/=/=");
+        MyLinkedList ll = new MyLinkedList(9);
+        ll.addFirst(11);
+        ll.addFirst(12);
+        ll.addFirst(13);
+
+        ll.add(4,9);
+        ll.add(4,9);
+        ll.printList();
+    }
+//    @Test
+//    void getData() {
+//    }
+//
+//    @Test
+//    void add() {
+//    }
+//
+//    @Test
+//    void addFirst() {
+//    }
+//
+//    @Test
+//    void get() {
+//    }
+//
+//    @Test
+//    void printList() {
+//    }
+}
