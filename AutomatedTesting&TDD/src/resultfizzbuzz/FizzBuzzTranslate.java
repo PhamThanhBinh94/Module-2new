f2f4c379bac8b067df2f6954fb7bd45f88aaedfd
thanhbinh2612 <thanhbinh261294@gmail.com>
2020-07-20 11:01:58 +0700

    Thực hành- Triển khai Queue sử dụng danh sách liên kết

---
+++ AutomatedTesting&TDD/src/resultfizzbuzz/FizzBuzzTranslate.java
@@ -0,0 +1,24 @@
+//package resultfizzbuzz;
+//
+//public class FizzBuzzTranslate {
+//    public  String countResult(int number){
+//        if (number>0){
+//            if (number%3==0)
+//            {
+//                return "Fizz";
+//            }
+//            if (number%5==0)
+//            {
+//                return "Buzz";
+//            }
+//            if (number%3==0 && number%5==0)
+//            {
+//                return "FizzBuzz";
+//            }
+//            if (number%3!=0 && number%5!=0)
+//            {
+//                return "number";
+//            }
+//        }
+//    }
+//}
---
+++ DSA-List/src/methodarraylist/MyList.java
@@ -0,0 +1,12 @@
+//package methodarraylist;
+//
+//public class MyList {
+//    private int size = 0;
+//
+//    public final int DEFAULT_CAPACITY = 10;
+//
+//    public MyList() {
+//    }
+//    public void add(int index, E e);
+//        add()
+//}
