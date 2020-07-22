c3bd8c153d44762a6ba9e662f6241ac029fb392d
thanhbinh2612 <thanhbinh261294@gmail.com>
2020-07-22 14:09:05 +0700

    Optional-Bài tập- Cài đặt thuật toán tìm kiếm nhị phân sử dụng đệ quy

---
+++ SearchingAlgorithms/src/baitaptimchuoitangdancododailonnhat/TimChuoiTangDanCoDoDaiLonNhat.java
@@ -0,0 +1,12 @@
+package baitaptimchuoitangdancododailonnhat;
+
+import java.util.Scanner;
+
+public class TimChuoiTangDanCoDoDaiLonNhat {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a string: ");
+        String string = input.nextLine();
+    }
+}
