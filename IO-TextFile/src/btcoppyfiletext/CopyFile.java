8fe3140135acf3102d611f83d422a629f5075d3c
thanhbinh2612 <thanhbinh261294@gmail.com>
2020-07-27 22:27:35 +0700

    Bài tập - Copy file text

---
+++ IO-TextFile/src/btcoppyfiletext/CopyFile.java
@@ -0,0 +1,27 @@
+package btcoppyfiletext;
+import java.io.*;
+
+public class CopyFile {
+
+    public static void main(String[] args)  {
+        try{
+            File file = new File("IO-TextFile\\data\\nguon.txt");
+            FileReader input = new FileReader(file);
+            File file2 = new File ("IO-TextFile\\data\\duoi.txt");
+            FileWriter output = new FileWriter(file2);
+            int line;
+            int count = 0;
+            while ((line=input.read())!=-1) {
+                System.out.println(line);
+                output.write(line);
+                count++;
+            }
+            output.close();
+            System.out.println("Total number of characters is: " + count);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
