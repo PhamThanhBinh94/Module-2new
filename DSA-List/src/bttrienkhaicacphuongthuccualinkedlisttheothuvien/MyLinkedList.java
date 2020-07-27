ea288417a17550a00b7db3d60704c8f8a79672c8
thanhbinh2612 <thanhbinh261294@gmail.com>
2020-07-27 11:50:51 +0700

    optional- Thực hành- Tính tổng các số trong file text

--- DSA-List/src/bttrienkhaicacphuongthuccualinkedlisttheothuvien/MyLinkedList.java
+++ DSA-List/src/bttrienkhaicacphuongthuccualinkedlisttheothuvien/MyLinkedList.java
@@ -31,6 +31,62 @@ public class MyLinkedList {
         head.next = temp;
         numNodes++;
     }
+    public void addLast(Object  data){
+        Node temp = head;
+        while (temp.next != null){
+            temp = temp.next;
+            temp.next = new Node(data);
+            numNodes++;
+        }
+    }
+
+    public void  add(int index , Object data){
+        Node temp = head;
+        Node holder;
+        if (index == 0){
+            addFirst(data);
+        } else  if (index ==numNodes){
+            addLast(data);
+        } else for (int i =0; i<index -1 && temp.next ==null;i++ ){
+            temp=temp.next;
+        }
+        holder = temp.next;
+        temp.next = new Node(data);
+        temp.next.next = holder;
+        numNodes++;
+    }
+    public void removeFirst(){
+        head = head.next;
+        numNodes--;
+    }
+    public void removeLast(){
+        Node temp = head;
+        int i =0;
+        while (i < numNodes-1){
+            temp = temp.next;
+            i++;
+        }
+        temp.next=null;
+        numNodes--;
+    }
+
+    public void remove(int index){
+        Node removeElement;
+        if (index ==0){
+            removeFirst();
+        } else if (index==numNodes){
+            removeLast();
+        } else {
+            removeElement = head;
+            int i = 0;
+            while (i < index -1){
+                removeElement = removeElement.next;
+                i++;
+            }
+            removeElement.next = removeElement.next.next;
+            numNodes--;
+        }
+    }
 
     public Node get(int index) {
         Node temp= head;
@@ -39,6 +95,15 @@ public class MyLinkedList {
         }
         return temp;
     }
+    public int size(){
+        Node temp = head;
+        int i = 1;
+        while (temp.next != null){
+            temp = temp.next;
+            i++;
+        }
+        return i;
+    }
 
     public void printList(){
         Node temp = head;
@@ -46,6 +111,29 @@ public class MyLinkedList {
             System.out.println(temp.data);
             temp = temp.next;
         }
+    }
+
+    public boolean contains(Object element){
+        Node temp = head;
+        while (temp.next != null){
+            if (element.equals(temp.data))
+                return true;
+            temp=temp.next;
+        }
+        return  false;
+    }
+
+    public int indexOf(Object element){
+        int index =0;
+        Node temp = head;
+        while (temp != null){
+            if (temp.data == element){
+                return index;
+            }
+            temp = temp.next;
+            index++;
+        }
+        return index;
     }
     @Override
     public String toString() {
