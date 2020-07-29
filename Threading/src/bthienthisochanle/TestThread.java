ad6a1e6a1b7d6b919748de980068fc70370e7e18
thanhbinh2612 <thanhbinh261294@gmail.com>
2020-07-29 21:28:45 +0700

    Bài tập- Hiển thị số chẵn /lẻ

---
+++ Threading/src/bthienthisochanle/TestThread.java
@@ -0,0 +1,15 @@
+package bthienthisochanle;
+
+public class TestThread {
+    public static void main(String[] args) {
+        OddThread oddThread = new OddThread();
+        EvenThread evenThread = new EvenThread();
+        oddThread.start();
+        try {
+            oddThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        evenThread.start();
+    }
+}
